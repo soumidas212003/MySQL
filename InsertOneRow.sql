e7af88eb5c1ac57f7e4c55ae1acf856089084ae5
Soumita Das <soumitad991@gmail.com>
2023-10-04 02:06:58 +0530

    Insert One row using sql

--- InsertOneRow.sql
+++ InsertOneRow.sql
@@ -1,6 +1,2 @@
--- insert into personal(id,name,birthdate,phone,gender)
--- values(2,"Soumita Das","2003-05-21","2365897410","F");
--- insert into personal(id,name,birthdate,phone,gender)
--- values(2,"Ruban Pathak","2002-11-09","7418529630","M");
 insert into personal(id,name,birthdate,phone,gender)
 values(2,"Sumi Dey","2003-08-09","7418852630","F");
